928c4ffba2c8c044aa99e6a15eed3ca977cd9f1b
elliotfouts <elliotfouts@gmail.com>
2020-07-01 20:53:44 -0700

    added user schema

--- models/users.js
+++ models/users.js
@@ -1,7 +1,24 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)/.test(email),
+      message: (email) => `${email.value} is not a valid email!`
+    }
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 const User = mongoose.model('user', UserSchema);
