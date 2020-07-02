0a40e4e7e56d1142d44b9b634eca7ffbe0900dd2
elliotfouts <elliotfouts@gmail.com>
2020-07-01 21:01:48 -0700

    added grocery controller

---
+++ controllers/groceries.js
@@ -0,0 +1,72 @@
+const {Grocery} = require('../models');
+
+// create methods
+const addGrocery = async (grocery) => {
+  // grocery format: { name, category, imageUrl, isCurrent, quantity, notes, users[] }
+  try {
+    let newGrocery = await Grocery.create(grocery);
+    return newGrocery;
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// read methods
+const getAllGroceries = async () => {
+  try {
+    const groceries = await Grocery.find({});
+    return groceries;
+  } catch (err) {
+    console.log(err)
+  }
+}
+const getCurrentGroceries = async () => {
+  try {
+    const currentGroceries = await Grocery.find({isCurrent: true});
+    return currentGroceries;
+  } catch (error) {
+    console.log(error)
+  }
+}
+const getSearchedGroceries = async (groceryName) => {
+  try {
+    const grocery = await Grocery.findOne({ "name" : { $regex: new RegExp(`${groceryName}`), $options: 'i' } });
+    return grocery;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// update methods 
+const updateGrocery = async (groceryInfo, groceryId) => {
+  try {
+    groceryInfo.set.size = groceryInfo.random;
+    const {set: $set, unset: $unset, push: $push, $pull} = groceryInfo;
+    const updateQuery = dropUndefined({$set, $unset, $push, $pull});
+
+    const updatedGrocery = await Grocery.findByIdAndUpdate(groceryId, updateQuery, {new: true});
+    return updatedGrocery;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// delete methods
+const deleteGrocery = async (groceryId) => {
+  try {
+    const deletedGrocery = await Grocery.findByIdAndDelete(groceryId);
+
+    return deletedGrocery;
+  } catch (err) {
+
+  }
+}
+
+module.exports = {
+  addGrocery, 
+  getAllGroceries,
+  getCurrentGroceries,
+  getSearchedGroceries,
+  updateGrocery,
+  deleteGrocery,
+}
--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-
+	Grocery: require('./groceries'),
+	User: require('./users')
 };
