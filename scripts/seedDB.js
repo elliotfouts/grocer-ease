c422980c317318f208893fbc28f8be86997ffb2e
elliotfouts <elliotfouts@gmail.com>
2020-07-01 20:38:50 -0700

    initial files

---
+++ scripts/seedDB.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/grocer-ease"
+);
+
+const PostSeed = [
+  // stuff
+];
+
+db.Post.remove({})
+  .then(() => db.Post.collection.insertMany(postSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
