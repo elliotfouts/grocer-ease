93a05dcfe8d9153ed3701bb492456d65ed81199d
elliotfouts <elliotfouts@gmail.com>
2020-07-01 20:50:45 -0700

    added grocery data model

---
+++ models/groceries.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const GrocerySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    default: "www.google.com"
+  },
+  isCurrent: {
+    type: Boolean,
+    required: true,
+  },
+  quantity: String,
+  note: String,
+  users: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'user'
+  }
+});
+
+const Grocery = mongoose.model('grocery', GrocerySchema);
+
+export default Grocery;
